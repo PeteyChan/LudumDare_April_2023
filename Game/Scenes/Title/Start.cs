b86ce5d296b06da36739ca8bc3a0daa347748a70
petey <akivsfyuu@gmail.com>
2023-04-29 16:25:53 +1000

    Added title scene

---
+++ Game/Scenes/Title/Start.cs
@@ -0,0 +1,14 @@
+using Godot;
+using System;
+
+public partial class Start : Button
+{
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        this.OnButtonDown(() =>
+        {
+			Scene.Load("res://Scenes/Game/Game.tscn");
+        });
+    }
+}
